c117c8ab6c928d5bb23056bc0d5fa26f9d446e2c
Quesses <quesses1@gmail.com>
2022-09-21 01:48:20 +0000

    python is fun

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    if (x >= 0):
+        begin_str = str[:n]
+        end_str = str[(n + 1):]
+        return (begin_str + end_str)
+    else:
+        return str
