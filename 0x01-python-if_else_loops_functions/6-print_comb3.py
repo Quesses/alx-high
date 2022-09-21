d8afe52b96dc9d012a2615fa939b1e91629ce3f2
Quesses <quesses1@gmail.com>
2022-09-21 01:19:00 +0000

    python is fun

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 for x in range(10):
     for y in range(10):
-        if (x < y) and not(x == 8) and not(y == 9):
-            print("{}{}, ".format(x, y), end="")
-        elif (x == 8) and (y == 9):
-            print("{}{}".format(x, y))
+        if (x < y):
+            if (x == 8) and (y == 9):
+                print("{}{}".format(x, y))
+            else:
+                print("{}{}, ".format(x, y), end="")
