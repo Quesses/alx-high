7884090fd0759b93d975295d8d2c2c4c93ac42d5
Quesses <quesses1@gmail.com>
2022-09-22 22:42:48 +0000

    python! pythongit add . && git commit -m

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import calculator_1, sys
+    arg_check = len(sys.argv[1:])
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    operator = sys.argv[2]
+    result = 0
+    if arg_check < 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    match operator:
+        case '+':
+            result = calculator_1.add(a, b)
+        case '-':
+            result = calculator_1.sub(a, b)
+        case '*':
+            result = calculator_1.mul(a, b)
+        case '/':
+            result = calculator_1.add(a, b)
+        case other:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+    print(f"{a} {operator} {b} = {result}")
