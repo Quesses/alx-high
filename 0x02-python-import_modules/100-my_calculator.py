2c3ac2a74b5745382dd6e187a73be17b491a471a
Quesses <quesses1@gmail.com>
2022-09-22 22:50:29 +0000

    python! pythongit add . && git commit -m

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,10 +2,10 @@
 if __name__ == "__main__":
     from sys import argv
     import calculator_1
-    arg_check = len(sys.argv[1:])
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    operator = sys.argv[2]
+    arg_check = len(argv[1:])
+    a = int(argv[1])
+    b = int(argv[3])
+    operator = argv[2]
     result = 0
     if arg_check < 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
