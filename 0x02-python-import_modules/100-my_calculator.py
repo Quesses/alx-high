698466b2697cf5602529467742bcd66b2be27fd9
Quesses <quesses1@gmail.com>
2022-09-22 23:04:28 +0000

    python! python

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,22 +3,23 @@ if __name__ == "__main__":
     from sys import argv
     from calculator_1 import add, sub, mul, div
     arg_check = len(argv[1:])
-    a = int(argv[1])
-    b = int(argv[3])
-    operator = argv[2]
     result = 0
     if arg_check < 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    elif operator == '+':
-        result = add(a, b)
-    elif operator == '-':
-        result = sub(a, b)
-    elif operator == '*':
-        result = mul(a, b)
-    elif operator == '/':
-        result = div(a, b)
     else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+        a = int(argv[1])
+        b = int(argv[3])
+        operator = argv[2]
+        if operator == '+':
+            result = add(a, b)
+        elif operator == '-':
+            result = sub(a, b)
+        elif operator == '*':
+            result = mul(a, b)
+        elif operator == '/':
+            result = div(a, b)
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
     print(f"{a} {operator} {b} = {result}")
