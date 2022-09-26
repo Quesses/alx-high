f21744282b99d2fa24f6aa7d677d805017a45492
Quesses <quesses1@gmail.com>
2022-09-26 01:04:22 +0000

    WOW! python

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for x in row:
-            print("{}".format(x), end=" ")
+    for row in range(len(matrix)):
+        for c in range(len(matrix[row])):
+            if c < len(matrix[row]) - 1:
+                print("{:d}".format(matrix[row][c]), end=" ")
+            else:
+                print("{:d}".format(matrix[row][c]), end="")
         print("")
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,14 +3,8 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
     else:
-        length = len(my_list) - 1
-        while length > 1:
-            index = 0
-            while index < length:
-                if my_list[index] < my_list[index + 1]:
-                    temp = my_list[index]
-                    my_list[index] = my_list[index + 1]
-                    my_list[index + 1] = temp
-            y += 1
-        x -= 1
-    return my_list[0]
+        max_int = my_list[0]
+        for x in my_list:
+            if x > max_int:
+                max_int = x
+        return max_int
