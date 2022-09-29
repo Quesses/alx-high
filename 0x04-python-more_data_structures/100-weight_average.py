8175d623daf61e689194cf873166810dbaed61ff
Quesses <quesses1@gmail.com>
2022-09-29 02:37:13 +0000

    python

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     if my_list == [] or my_list is None:
-        return None
+        return 0
     sum_xy = sum(map(lambda x: x[0] * x[1], my_list))
     sum_y = sum(map(lambda y: y[1], my_list))
     return sum_xy / sum_y
