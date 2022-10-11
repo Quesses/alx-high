eeff9f4b672901ca958c7d552452e9e1c0338405
Quesses <quesses1@gmail.com>
2022-10-11 22:02:57 +0000

    task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/phyton3
-class Square:
-    """An empty class that defines a square.
+"""A Square class to practice class and docstring style coding."""
 
-    """
+
+class Square:
+    """An empty class that defines a square."""
     pass
