5cc04961b495dd18e0a131a172d729f0e25d6a1c
Quesses <quesses1@gmail.com>
2022-10-12 00:09:35 +0000

    task 1 & 2

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -11,9 +11,8 @@ class Square:
         No type / value verification
         """
 
+        self.__size = size
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("sixe must be >= 0")
-
-        self.__size = size
