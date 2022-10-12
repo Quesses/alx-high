b8a2f62faf547c04ef3f5641e373ae74be7d6542
Quesses <quesses1@gmail.com>
2022-10-12 00:22:33 +0000

    task 1 & 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,11 +9,12 @@ class Square:
         """
         Initialize the square with a size (private instantiation)
         No type / value verification
+
+        param size: integer value > 0.
         """
 
+        self.__size = size
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("sixe must be >= 0")
-
-        self.__size = size
