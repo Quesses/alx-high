304d69712daf69761b5c81a71bc0aee2c0c05c60
Quesses <quesses1@gmail.com>
2022-12-29 23:03:59 +0000

    done

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 function factorial (x) {
   if (x < 0) {
-    return (-1)
-  }
+    return (-1);
+  };
   if (x === 0 || isNaN(x)) {
-    return (1)
-  }
-  return (x * factorial(x - 1))
-}
+    return (1);
+  };
+  return (x * factorial(x - 1));
+};
 
-const result = factorial(Number(process.argv[2]))
-console.log(result)
+const result = factorial(Number(process.argv[2]));
+console.log(result);
