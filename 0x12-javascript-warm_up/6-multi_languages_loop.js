7f150733806d408e4509b03cfa322898caf4719e
Quesses <quesses1@gmail.com>
2022-12-29 22:57:22 +0000

    done

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const arrayStr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let x of arrayStr) {
-	console.log(x);
+const arrayStr = ['C is fun', 'Python is cool', 'JavaScript is amazing']
+for (const x of arrayStr) {
+  console.log(x)
 }
